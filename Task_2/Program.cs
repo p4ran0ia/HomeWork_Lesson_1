f3871b21fa06e25b840080f833af2e7f5843db30
Alexandr <crazy.top@mail.ru>
2022-09-01 14:58:16 +0300

    Task complete

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > a)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
