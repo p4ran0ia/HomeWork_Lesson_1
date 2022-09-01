d173075a463375faa735021827b53cb324b9de89
Alexandr <crazy.top@mail.ru>
2022-09-01 15:43:10 +0300

    Task Complete

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while(i <= N)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i+" ");
+    }
+    i++;
+}
+
+
