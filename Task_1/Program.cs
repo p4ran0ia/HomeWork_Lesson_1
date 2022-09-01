1807d794553a126379c7e753782703c65639167a
Alexandr <crazy.top@mail.ru>
2022-09-01 14:45:10 +0300

    Task complete

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Input numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+    Console.WriteLine(a + " max " +  b + " min");
+}
+if(a < b)
+{
+    Console.WriteLine(b + " max " +  a + " min");
+}
+if(a == b)
+{
+    Console.WriteLine("the numbers are equal");
+}
+
+
